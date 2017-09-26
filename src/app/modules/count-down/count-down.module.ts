43b92f718292db62010858c73013c11faa44fa9f
Hammad Maqbool <hammad417@gmail.com>
2017-09-26 16:33:01 +0500

    added new modules folder, which contain count-down module and generate new count-down component, making  plugin structure

---
+++ src/app/modules/count-down/count-down.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CountDownComponent } from './count-down.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CountDownComponent]
+})
+export class CountDownModule { }
